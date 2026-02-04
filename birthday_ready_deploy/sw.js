5be2968211e9dd47865d67c043eb44091c2853ef
Raushan192003 <165303739+Raushan192003@users.noreply.github.com>
2026-02-04 20:39:35 +0530

    Add files via upload

---
+++ birthday_ready_deploy/sw.js
@@ -0,0 +1,8 @@
+
+const CACHE_NAME = "birthday-v1";
+self.addEventListener("install", e=>{
+  e.waitUntil(caches.open(CACHE_NAME));
+});
+self.addEventListener("fetch", e=>{
+  e.respondWith(fetch(e.request));
+});
